d7b0e4357ee1aacc4dfc4ec9e01ffea51069f98a
htikkke <htikew574@gmail.com>
2026-05-29 14:11:58 +0630

    chore: integrate tailwind v4 with vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "/Interactive_Rating_Component_Frontend_Mentor/",
 });
